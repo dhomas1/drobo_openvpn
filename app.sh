2522e16140073fe6bd72b463fe05ff9fd07ad84a
dhomas1 <dom@cotugno.co>
2024-05-08 08:54:03 -0400

    Update app.sh sources

--- app.sh
+++ app.sh
@@ -1,9 +1,11 @@
 ### PCRE ###
 _build_pcre() {
-local VERSION="8.39"
+#local VERSION="8.39"
+local VERSION="8.45"
 local FOLDER="pcre-${VERSION}"
 local FILE="${FOLDER}.tar.bz2"
-local URL="ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/${FILE}"
+#local URL="ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/${FILE}"
+local URL="https://sourceforge.net/projects/pcre/files/pcre/${VERSION}/${FILE}"
 
 _download_bz2 "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/${FOLDER}"
@@ -17,10 +19,12 @@ popd
 
 ### LIBSEPOL ###
 _build_libsepol() {
-local VERSION="2.4"
+#local VERSION="2.4"
+local VERSION="3.6"
 local FOLDER="libsepol-${VERSION}"
 local FILE="${FOLDER}.tar.gz"
-local URL="https://raw.githubusercontent.com/wiki/SELinuxProject/selinux/files/releases/20150202/${FILE}"
+#local URL="https://raw.githubusercontent.com/wiki/SELinuxProject/selinux/files/releases/20150202/${FILE}"
+local URL="https://github.com/SELinuxProject/selinux/releases/download/${VERSION}/${FILE}"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/${FOLDER}"
@@ -33,10 +37,12 @@ popd
 ### LIBSELINUX ###
 # requires pcre, libsepol
 _build_libselinux() {
+#local VERSION="2.4"
 local VERSION="2.4"
 local FOLDER="libselinux-${VERSION}"
 local FILE="${FOLDER}.tar.gz"
-local URL="https://raw.githubusercontent.com/wiki/SELinuxProject/selinux/files/releases/20150202/${FILE}"
+#local URL="https://raw.githubusercontent.com/wiki/SELinuxProject/selinux/files/releases/20150202/${FILE}"
+local URL="https://github.com/SELinuxProject/selinux/releases/download/${VERSION}/${FILE}"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/${FOLDER}"
@@ -48,7 +54,7 @@ popd
 
 ### LZO ###
 _build_lzo() {
-local VERSION="2.09"
+local VERSION="2.10"
 local FOLDER="lzo-${VERSION}"
 local FILE="${FOLDER}.tar.gz"
 local URL="http://www.oberhumer.com/opensource/lzo/download/${FILE}"
@@ -64,14 +70,14 @@ popd
 
 ### ZLIB ###
 _build_zlib() {
-local VERSION="1.2.8"
+local VERSION="1.3.1"
 local FOLDER="zlib-${VERSION}"
 local FILE="${FOLDER}.tar.gz"
 local URL="http://zlib.net/${FILE}"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/${FOLDER}"
-./configure --prefix="${DEPS}" --libdir="${DEST}/lib"
+./configure --prefix="${DEPS}" --libdir="${DEST}/lib" --shared
 make
 make install
 rm -v "${DEST}/lib"/*.a
@@ -80,15 +86,13 @@ popd
 
 ### OPENSSL ###
 _build_openssl() {
-local VERSION="1.0.2h"
+local VERSION="1.1.1w"
 local FOLDER="openssl-${VERSION}"
 local FILE="${FOLDER}.tar.gz"
-local URL="http://mirror.switch.ch/ftp/mirror/openssl/source/${FILE}"
+local URL="http://www.openssl.org/source/${FILE}"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
-cp -vf "src/${FOLDER}-parallel-build.patch" "target/${FOLDER}/"
 pushd "target/${FOLDER}"
-patch -p1 -i "${FOLDER}-parallel-build.patch"
 ./Configure --prefix="${DEPS}" --openssldir="${DEST}/etc/ssl" \
   zlib-dynamic --with-zlib-include="${DEPS}/include" --with-zlib-lib="${DEPS}/lib" \
   shared threads linux-armv4 -DL_ENDIAN ${CFLAGS} ${LDFLAGS} \
@@ -100,7 +104,7 @@ mkdir -p "${DEST}/libexec"
 cp -vfa "${DEPS}/bin/openssl" "${DEST}/libexec/"
 cp -vfa "${DEPS}/lib/libssl.so"* "${DEST}/lib/"
 cp -vfa "${DEPS}/lib/libcrypto.so"* "${DEST}/lib/"
-cp -vfaR "${DEPS}/lib/engines" "${DEST}/lib/"
+cp -vfaR "${DEPS}/lib/engines"* "${DEST}/lib/"
 cp -vfaR "${DEPS}/lib/pkgconfig" "${DEST}/lib/"
 rm -vf "${DEPS}/lib/libcrypto.a" "${DEPS}/lib/libssl.a"
 sed -e "s|^libdir=.*|libdir=${DEST}/lib|g" -i "${DEST}/lib/pkgconfig/libcrypto.pc"
@@ -110,9 +114,10 @@ popd
 
 ### OPENVPN ###
 _build_openvpn() {
-local VERSION="2.3.8"
+local VERSION="2.6.10"
 local FOLDER="openvpn-${VERSION}"
 local FILE="${FOLDER}.tar.gz"
+#local URL="https://swupdate.openvpn.org/community/releases/${FILE}"
 local URL="https://swupdate.openvpn.org/community/releases/${FILE}"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
